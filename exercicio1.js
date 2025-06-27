acaaf6082b214fad724b864fc21c73f815b953d4
Augusto Brigida Silva <augustobrigida060@gmail.com>
2025-06-27 18:29:43 -0300

    add:correção exercicio1

--- exercicio1.js
+++ exercicio1.js
@@ -1,1 +1,16 @@
-a =  2 + 2
+//EXERCÍCIO 1:
+let a = parseInt(prompt())
+let b = parseInt(prompt())
+let c = parseInt(prompt())
+
+let delta = (b**2) - 4*a*c
+if(delta > 0){
+   x1 = (-b + Math.sqrt(delta)) / 2*a
+   x2 = (-b - Math.sqrt(delta)) / 2*a
+   console.log(`As raízes da equação são ${x1} e ${x2}`)
+}else if(delta === 0){
+       x1 = (-b + Math.sqrt(delta)) / 2*a
+       console.log(`A raíz da equação é ${x1}`)
+}else{
+    console.log(`A equação não possui raízes reais`)
+}
